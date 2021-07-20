0d4703e0aa58e8c946141f042d443a5aa84638fc
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-20 09:02:11 +0000

    samtools: update 1.13 bottle.

--- Formula/samtools.rb
+++ Formula/samtools.rb
@@ -10,6 +10,7 @@ class Samtools < Formula
     sha256 cellar: :any,                 big_sur:       "98634d489c88407d061c93da8b5865da65e36fd40e4a08ae505381ffb1b9fa52"
     sha256 cellar: :any,                 catalina:      "4b4ba1ac799d038a0df5fecd446ab6ad4e331e6682ce4a372b893b91d07f3ada"
     sha256 cellar: :any,                 mojave:        "05287c31f59b12f136c3da0f84cfe2be5c59275a3f66163736e11d231839e111"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b4d7af3be41b01c7697992e31dc1f4d289e440e3c34da87e5064552f69e4afe"
   end
 
   depends_on "htslib"
