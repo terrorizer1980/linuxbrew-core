a06eb344d1b60fcc6eeb10705ed171d8c2afba7c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-21 08:35:23 +0000

    buildkit: update 0.9.0 bottle.

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -17,6 +17,7 @@ class Buildkit < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "76db05e2e242bdc332b0ece0c22c28cc2b1509a6c57b83516bf9700c71d27566"
     sha256 cellar: :any_skip_relocation, catalina:      "f4a58c4aa6401635fba676aa36ddfb0e91dc6fa10c7246a0210e8dbf85f688db"
     sha256 cellar: :any_skip_relocation, mojave:        "e7dee5f840be67e86e4e8a252ce18e1bccb5dc70385184be0b58b2f312f962ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1cc283a28854f3a2ac0a47f3a5be50b18799408516aa45ec90f73e18bd755f5b"
   end
 
   depends_on "go" => :build
