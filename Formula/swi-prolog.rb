d018717c67f487b7d8ef495d2034934e35b6e84e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-24 15:30:48 +0000

    swi-prolog: update 8.2.4 bottle.

--- Formula/swi-prolog.rb
+++ Formula/swi-prolog.rb
@@ -16,6 +16,7 @@ class SwiProlog < Formula
     sha256 big_sur:       "1a5384ec31ca1088eff1e16d1977a4120f12e8c15e3703ef3c80c564848e2e5c"
     sha256 catalina:      "06c31b007436027785e73cdbf16d82dd6356766b821580a4fceda8db5eb4c86c"
     sha256 mojave:        "1deeaab4064bf3da632a28e539f46790252cfec839ae932bc729ecc239e347c3"
+    sha256 x86_64_linux:  "e6e69b8d32e9cb03ce72e76cd1e96f99fc2b5b1401ff64acdb94c9a4aba458ff"
   end
 
   depends_on "cmake" => :build
