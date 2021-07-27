3be020a81e81761ba69f5e29f4656a107d5ce74d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-27 12:25:14 +0000

    nifi: add 1.14.0 bottle.

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -6,6 +6,10 @@ class Nifi < Formula
   sha256 "858e12bce1da9bef24edbff8d3369f466dd0c48a4f9892d4eb3478f896f3e68b"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "adaa43b0ec0370e2f011d02c934180380f4efe0bce1105e1259833cdb1ec6cb4"
+  end
+
   depends_on "openjdk@11"
 
   def install
