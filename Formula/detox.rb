73985857377c98c3944616c1dab702c30cd913ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-27 12:15:51 +0000

    detox: update 1.4.3 bottle.

--- Formula/detox.rb
+++ Formula/detox.rb
@@ -10,6 +10,7 @@ class Detox < Formula
     sha256 big_sur:       "83f10052d44f3c4c0fd6fd96a24acde6c5babed73a241c21e5df28e1d15327ba"
     sha256 catalina:      "b19a68972142d3c4392d38e2b6d32e2b63767b0d3faf8d27cb8b0197eb50b820"
     sha256 mojave:        "f063f567cc0cde45b92ee8ab37481259aa832e9e117c96927a2838be6bfc90af"
+    sha256 x86_64_linux:  "502f29bba32a3f877ea56f1a2ea2d8fd6f26a4a8eb181e4d69d19a2d1adde637"
   end
 
   depends_on "autoconf" => :build
