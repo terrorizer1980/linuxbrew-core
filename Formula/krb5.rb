035e5b9507ce18c25597e03e2dae75cc889e92e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-27 15:22:31 +0000

    krb5: update 1.19.2 bottle.

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -15,6 +15,7 @@ class Krb5 < Formula
     sha256 big_sur:       "6ae8cffb08f9cba4842a21fc92618e22403a474fbd6d05820c3755401d55688d"
     sha256 catalina:      "5d0157ff13610f06bc262350978ef1f754e2b7ed0721ba27c431cfd5b5b16639"
     sha256 mojave:        "9dd6fca906e634be1c1b05a99fa8f28735a969e3eb4d939048e89f322c3a4278"
+    sha256 x86_64_linux:  "2626ed3b8a8e5448aabeac56f6fe91b199fe896a5fd84394d5475213fde1a139"
   end
 
   keg_only :provided_by_macos
