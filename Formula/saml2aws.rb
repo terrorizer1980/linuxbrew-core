e6b4a310ea0515bac046843e7a91db9b60c3aa1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-29 15:01:43 +0000

    saml2aws: update 2.32.0 bottle.

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -11,6 +11,7 @@ class Saml2aws < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "f8400874432bf2363cf66a350d823b003d8168e0619768fe762cc5a2c3b5dddb"
     sha256 cellar: :any_skip_relocation, catalina:      "b18ff9fd260e6f234d7e1bfe16baef4ef64a6987a8b04831afc13f83a7bcfcd6"
     sha256 cellar: :any_skip_relocation, mojave:        "cf22563c79eb2e6bd479124ba520b40dac75f93ce97e3128e7fd16629f450c57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "22a26167391c2288b940e20727acf7f9c8e0b6602c76d1a7410b69d6a0d6078d"
   end
 
   depends_on "go" => :build
