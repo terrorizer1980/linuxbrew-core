65717b6f83d3600c74fa8a92c04569dcf0b4dade
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-01 11:56:06 +0000

    mdbtools: update 0.9.3_1 bottle.

--- Formula/mdbtools.rb
+++ Formula/mdbtools.rb
@@ -11,6 +11,7 @@ class Mdbtools < Formula
     sha256 cellar: :any,                 big_sur:       "92ff7689be581613ec6dfb68a53246925a65b81f38902d081a15520d4cc7ea12"
     sha256 cellar: :any,                 catalina:      "ac2ef7a275f74d9372cea1a7e0012abdcab67ae4caf6c6aaf1b60d92ced9b3c5"
     sha256 cellar: :any,                 mojave:        "aa0dd1053890f163b19aeb7ca338040762cf2c499f9f629c390e57311a2d4cf0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0936835475319db278156fd8bbe5acb7538e9957f3c4e997f137e4fbcc2baa71"
   end
 
   depends_on "autoconf" => :build
