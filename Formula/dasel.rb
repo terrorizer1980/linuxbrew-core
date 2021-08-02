5d6a3f418e77e3d821ceb185c1d72312cb0659cf
Michka Popoff <michkapopoff@gmail.com>
2021-08-02 12:46:40 +0200

    formulae: add comment to bottle line to generate conflicts on next merge

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -11,7 +11,7 @@ class Dasel < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d9bb259933a2f24df633163b88bbb66c14093a58476134e5adda323bdf543e94"
     sha256 cellar: :any_skip_relocation, catalina:      "a397d678cd5f1d5264ae718eadf5e0a26dc194ef1cc7e9dff730eaf34c0759f6"
     sha256 cellar: :any_skip_relocation, mojave:        "582a698518311b599a4913b115f2765a6b2770278d47fd8e2a6079f49c9e1304"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e87e00fc512f8a4fdf7f8f3eda9c9b1278f0a0e5313437485680496c59480d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e87e00fc512f8a4fdf7f8f3eda9c9b1278f0a0e5313437485680496c59480d9" # linuxbrew-core
   end
 
   depends_on "go" => :build
