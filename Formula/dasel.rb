432084c6b150323b2269460a106a532637b34d03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-02 18:53:46 +0000

    dasel: update 1.16.1 bottle.

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -11,6 +11,7 @@ class Dasel < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "f507fd2a396cb489e54b904b719ca05dcba0e8a199eda411a732aecff407c2ed"
     sha256 cellar: :any_skip_relocation, catalina:      "da6eb4609a572b33f6147e616a5a1f65f1ed22bb2a6cb4ee22d1f24b7de1ae09"
     sha256 cellar: :any_skip_relocation, mojave:        "a04c05d640624c4b46542ef3f313c73ed08894287a8dc353185e97af8c3ce883"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1cb597ff0f91895b0a231db684309b4d607677be1c13b44a426a85ba3737e1aa"
   end
 
   depends_on "go" => :build
