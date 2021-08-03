db0408a6295059556e1c80556e94166cef7022c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-03 19:35:37 +0000

    flarectl: update 0.20.0 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -11,6 +11,7 @@ class Flarectl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d797f82595302d4240955988835ca23ab59a8d2894995667110ee8e5dd8392fd"
     sha256 cellar: :any_skip_relocation, catalina:      "6ad60831572fafc18c5a89a52acd0ed76a858d69372eff819d2a90fe54f3da05"
     sha256 cellar: :any_skip_relocation, mojave:        "b9f01eae281325c595e882a023ee4d2c5a1445c330a6ba82060e2c4cfa47668c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0047137a5fefe80aecdb60ba6aa28f1e70b5a2361d7292bc45d77511b920d16e"
   end
 
   depends_on "go" => :build
