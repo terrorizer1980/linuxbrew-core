fff151f3206a34a427d74e78fda4c2398b19914e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-05 07:05:21 +0000

    fuse-overlayfs: add 1.7 bottle.

--- Formula/fuse-overlayfs.rb
+++ Formula/fuse-overlayfs.rb
@@ -5,6 +5,10 @@ class FuseOverlayfs < Formula
   sha256 "e4d9a794d270e237a38e7ced95af95ad15268e0584eab981ed7c7b3758b95995"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d4b0aeed4dceb0f4083d84209eda9f2ac0a70e3c67a8824d3594b298e2bc7bec"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
