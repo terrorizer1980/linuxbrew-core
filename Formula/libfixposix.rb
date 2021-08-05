da8155a537e7165aa3bfb20df88e4f31104cd5d6
Daniel Nachun <daniel.nachun@gmail.com>
2021-08-05 12:42:56 -0700

    libfixposix: align with upstream (#24096)

--- Formula/libfixposix.rb
+++ Formula/libfixposix.rb
@@ -39,13 +39,12 @@ class Libfixposix < Formula
       int main(void)
       {
           fd_set rset, wset, eset;
-          unsigned i;
 
           lfp_fd_zero(&rset);
           lfp_fd_zero(&wset);
           lfp_fd_zero(&eset);
 
-          for(i = 0; i < FD_SETSIZE; i++) {
+          for(unsigned i = 0; i < FD_SETSIZE; i++) {
               if(lfp_fd_isset(i, &rset)) {
                   printf("%d ", i);
               }
