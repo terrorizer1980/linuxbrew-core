4f031b2750f69bc71fcc8e12fec0e44fd6ef7047
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-05 07:09:15 +0000

    php-cs-fixer: add 3.0.2 bottle.

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -5,6 +5,10 @@ class PhpCsFixer < Formula
   sha256 "b537c19425b0e638c2bebcffee84eeed5670b29ebbb44219d7b675a5c2f7088d"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "62b2f978a9b90740712983a5891dc1c7843f9b3b18a05a62ff7c17b5728db44f"
+  end
+
   uses_from_macos "php", since: :mojave
 
   def install
