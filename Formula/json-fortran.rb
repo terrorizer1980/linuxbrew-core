f7bb926812eb97bf4927f236cb93a7dcf45b4d39
Daniel Nachun <daniel.nachun@gmail.com>
2021-08-05 22:59:59 -0700

    json-fortran: remove unneeded dependency (#24116)

--- Formula/json-fortran.rb
+++ Formula/json-fortran.rb
@@ -18,10 +18,6 @@ class JsonFortran < Formula
   depends_on "ford" => :build
   depends_on "gcc" # for gfortran
 
-  on_linux do
-    depends_on "libx11"
-  end
-
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args,
