059ba76c58d7c6ea0d330a70fe926c42c75153b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-06 10:14:57 +0000

    zoxide: update 0.7.3 bottle.

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -10,6 +10,7 @@ class Zoxide < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "8e6a693a7672853ad0d148bf388fbff77620fd1e712e032099e907aeb793ceca"
     sha256 cellar: :any_skip_relocation, catalina:      "a7f3688ad3276e53c10cfb2441d8817965eb9f2c4a37df5bb5e469c963855d66"
     sha256 cellar: :any_skip_relocation, mojave:        "809cc928260685cd3517f73d38f9d4dbab62c41f07a2d3c2cf5259567bbf0671"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ec85437a8cda720b5a001df1429a5c62017b3b0a6c3f211c9e8ab22b266eee8"
   end
 
   depends_on "rust" => :build
