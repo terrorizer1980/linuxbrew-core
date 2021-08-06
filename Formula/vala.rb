7757a5c1a55e093b23f24cc5862a8590ab222283
Daniel Nachun <daniel.nachun@gmail.com>
2021-08-05 23:09:55 -0700

    vala: remove unneeded dependency (#24125)

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -21,10 +21,6 @@ class Vala < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
-  on_linux do
-    depends_on "libx11"
-  end
-
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
