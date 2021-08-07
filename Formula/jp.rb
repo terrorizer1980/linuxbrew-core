692c22593442075683bba8b0eef4b73e8aabda28
Michka Popoff <michkapopoff@gmail.com>
2021-08-07 18:53:16 +0200

    jp: add patch back after merge

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -40,3 +40,20 @@ class Jp < Formula
     pipe_output("#{bin}/jp -input csv -xy '[*][0,1]'", "0,0\n1,1\n", 0)
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index adc9d13..664b6af 100644
+--- a/Makefile
++++ b/Makefile
+@@ -90,3 +90,10 @@ release/$(APP)_$(VERSION)_linux_arm64.zip: binaries/linux_arm64/$(APP)
+ 
+ binaries/linux_arm64/$(APP): $(GOFILES)
+ 	GOOS=linux GOARCH=arm64 go build -ldflags "-X main.version=$(VERSION)" -o binaries/linux_arm64/$(APP) ./cmd/$(APP)
++
++release/$(APP)_$(VERSION)_osx_arm64.zip: binaries/osx_arm64/$(APP)
++	mkdir -p release
++	cd ./binaries/osx_arm64 && zip -r -D ../../release/$(APP)_$(VERSION)_osx_arm64.zip $(APP)
++
++binaries/osx_arm64/$(APP): $(GOFILES)
++	GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$(VERSION)" -o binaries/osx_arm64/$(APP) ./cmd/$(APP)
