95527dcb1b6fade6aed5c5879051d52ecc655ede
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-08 17:14:43 +0000

    liquibase: add 4.4.3 bottle.

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -5,6 +5,10 @@ class Liquibase < Formula
   sha256 "b5dfa605ffc9853c39bef96ea72965a59d9ee12a3bcb59040b539d4782d4bff9"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7469e0579ada8574a828d71ce5911bd2f7a5c52cd5dcaa0b75fd82b7f86422ab"
+  end
+
   depends_on "openjdk"
 
   def install
