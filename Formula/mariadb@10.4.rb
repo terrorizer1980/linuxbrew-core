e89a4045c24f7ed64f4cb732248746671056e19d
Daniel Nachun <daniel.nachun@gmail.com>
2021-08-08 23:16:30 -0700

    mariadb@10.4: align with upstream (#24160)

--- Formula/mariadb@10.4.rb
+++ Formula/mariadb@10.4.rb
@@ -170,7 +170,7 @@ class MariadbAT104 < Formula
     EOS
   end
 
-  plist_options manual: "${HOMEBREW_PREFIX}/opt/mariadb@10.4/bin/mysql.server start"
+  plist_options manual: "mysql.server start"
 
   def plist
     <<~EOS
