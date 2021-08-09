9166c53fc471cb23284a648164382e0470de1748
Daniel Nachun <daniel.nachun@gmail.com>
2021-08-09 04:30:11 -0700

    gcc@5: align with upstream (#24196)

--- Formula/gcc@5.rb
+++ Formula/gcc@5.rb
@@ -1,5 +1,3 @@
-require "os/linux/glibc"
-
 class GccAT5 < Formula
   desc "GNU Compiler Collection"
   homepage "https://gcc.gnu.org/"
@@ -13,7 +11,6 @@ class GccAT5 < Formula
     regex(%r{href=.*?gcc[._-]v?(5(?:\.\d+)+)(?:/?["' >]|\.t)}i)
   end
 
-  # gcc is designed to be portable.
   bottle do
     sha256 cellar: :any, high_sierra:  "dcc9059b725fd7c87842287bbedf60a28745417652d42a300dcd944e15986f36"
     sha256               x86_64_linux: "cd94b6bc2189df7861c2c32c480f777984865dbab4107f493188feda5a05b80d" # linuxbrew-core
