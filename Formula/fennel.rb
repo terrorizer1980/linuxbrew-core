ad92ebb2f7c678f548a55bce4aa85de843a3a2e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-15 10:36:46 +0000

    fennel: add 0.10.0 bottle.

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -5,6 +5,10 @@ class Fennel < Formula
   sha256 "407d47ae50e9d7a756f47b7bd81c4bdcb0b24c40322b496e7fe7357ae1eee6e3"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "61413fc779425837a7498ece98d85016b3a5d26f1ffb5099f6363afe058b2290"
+  end
+
   depends_on "lua"
 
   def install
