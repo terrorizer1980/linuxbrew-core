93610313de72a03a3c66215e0d6d2eb803ec944f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-16 19:01:37 +0000

    ormolu: update 0.2.0.0 bottle.

--- Formula/ormolu.rb
+++ Formula/ormolu.rb
@@ -11,6 +11,7 @@ class Ormolu < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "151d4b1c54440eaf96b72a0b417894d3fe4ca2478eb94a242b3abe144498ea68"
     sha256 cellar: :any_skip_relocation, catalina:      "cdf06624755fe56202d3f0aa504fe0402ada1189e44de0d60a53a8cd7901e83b"
     sha256 cellar: :any_skip_relocation, mojave:        "3e54ed98fd90c029f523657d543bea53ca67d37918144452846dfa059c3d461d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d88af1cb208b7524dbd97d495af34d0cb64d5db23ad355f97cabe6008a19292"
   end
 
   depends_on "cabal-install" => :build
