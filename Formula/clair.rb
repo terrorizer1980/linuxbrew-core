21b9cd75cc68ede9455587e9cd8ac8f38d03a18a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-18 19:56:39 +0000

    clair: update 4.2.2 bottle.

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -14,6 +14,7 @@ class Clair < Formula
     sha256 cellar: :any_skip_relocation, big_sur:      "b5faebb8f68c655915947dba54810d42e190c6f5968daeebd2ddf4bbe0f6f578"
     sha256 cellar: :any_skip_relocation, catalina:     "a99136e4561adbdd70cb68270035ae49ef38ced0ad76c75e215b4c27e12f7df3"
     sha256 cellar: :any_skip_relocation, mojave:       "d6af1310160626469d81d9f14ee5c247bcb913f3c984210e444a2607a66d2bbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b60e0f16d3c41891a652aa8eb4878d203153cbc6502791666c058ab9f21cf281"
   end
 
   depends_on "go" => :build
