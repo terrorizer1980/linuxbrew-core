9828831db0538ad023fb84570881bbf2c372ca55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-18 19:10:44 +0000

    singularity: add 3.8.1 bottle.

--- Formula/singularity.rb
+++ Formula/singularity.rb
@@ -5,6 +5,10 @@ class Singularity < Formula
   sha256 "2648366fbc7e8fcba9d32f8f187ea149356c5f9083a57826d641359fa8415d9a"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8579e2228ef36fe875618f5f16d99c417a57093762b99961c82d68789588f3c6"
+  end
+
   depends_on "go" => :build
   depends_on "openssl@1.1" => :build
   depends_on "libarchive"
