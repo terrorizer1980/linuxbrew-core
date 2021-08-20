fa683ac144f962e09986e17549e040cb33b67e4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-20 19:40:36 +0000

    osslsigncode: update 2.2 bottle.

--- Formula/osslsigncode.rb
+++ Formula/osslsigncode.rb
@@ -10,6 +10,7 @@ class Osslsigncode < Formula
     sha256 cellar: :any,                 big_sur:       "6e61b636105463514c06a1a85b69fc5c6a6820ca37c52bd77d9183ef43c18048"
     sha256 cellar: :any,                 catalina:      "c9f8e8e0e6f50ee007996a837d369fd86a420d3b33ad96f93147e33b2c9e8b2a"
     sha256 cellar: :any,                 mojave:        "673f0150f56426a6218d02743c1b3ed3e564a0bb454d335eda0f1795dd513dd1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68479dd084bd4a06ccba9a64756904b6ffea34d49acb93384371778b316ae07e"
   end
 
   depends_on "autoconf" => :build
