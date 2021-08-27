8d9adc0b9edaf8ef4f88bdf9b125eb283bf331b3
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-08-27 12:10:44 +0000

    typescript: add 4.4.2 bottle.

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -8,6 +8,10 @@ class Typescript < Formula
   license "Apache-2.0"
   head "https://github.com/Microsoft/TypeScript.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4601871ab9f9380461b06d9451122f7573d07e5f569aaedaadad09cd815e5925"
+  end
+
   depends_on "node"
 
   def install
