38a659ceade0b0d05669aded610f7bd9db1be6ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-28 20:40:25 +0000

    svgo: add 2.5.0 bottle.

--- Formula/svgo.rb
+++ Formula/svgo.rb
@@ -7,6 +7,10 @@ class Svgo < Formula
   sha256 "a3cb6e095c43a21ea2317f00e48b10b41ad729b96bf11c1846a3d3b25dc527e0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "67398482ddb88f63a3dcb29db1c5e06b918c8ba6acfc6d05257a0c19a1d285be"
+  end
+
   depends_on "node"
 
   def install
