ea36b1a9ca5930e3d5d18cc70626c66a13df6b3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-30 17:43:24 +0000

    crun: add 1.0 bottle.

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -5,6 +5,10 @@ class Crun < Formula
   sha256 "1db52e6a9c34d44dc49da6a35ac989d59865cec3267e443696f0ed1f4db31103"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "293682e023593aa545abcf654fe6152b4da929140a0dbddede2127e519669b91"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "go-md2man" => :build
