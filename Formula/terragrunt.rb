1e832e2bcbed6b7ca583d7e5cf35d130de033317
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-31 05:25:11 +0000

    terragrunt: update 0.31.8 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -10,6 +10,7 @@ class Terragrunt < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d54915f105fc5376214f7025a084065144851510bb15a7becc4d73661d805b45"
     sha256 cellar: :any_skip_relocation, catalina:      "08b16bc2e39870d9ad122bd9e33632c3105d84d77bc2ad326b471ef9b5e29ad7"
     sha256 cellar: :any_skip_relocation, mojave:        "f6351d2f916bc3a3062664da55e50bc52b12b1d53ccd19745e44106e3f7f1a4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6953b9795123adf5635dca9716b35fc687d08bccffb694f19cfd284508b65295"
   end
 
   depends_on "go" => :build
