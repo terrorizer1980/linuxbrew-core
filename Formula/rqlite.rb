e2645f043a0d7b892bc23b12f96fa4121c077229
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-31 19:51:29 +0000

    rqlite: update 6.4.0 bottle.

--- Formula/rqlite.rb
+++ Formula/rqlite.rb
@@ -11,6 +11,7 @@ class Rqlite < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "2ac2ba940ca7a3b4e1e6e02823bf4820a12e3f49bf7d2a6febf08dd4fb66ff6a"
     sha256 cellar: :any_skip_relocation, catalina:      "f78e7a559a44cb937407c9192c043947734f865329d1af298ceff3357f86d489"
     sha256 cellar: :any_skip_relocation, mojave:        "a37b63da450742e68623ecc3ba4ad2ca865439470b29f98c27ca994a626191f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c374527a9133267e88d10aa66d39099aef6e8c0073b3414e79b53e1cb5561471"
   end
 
   depends_on "go" => :build
