1e7ce24506d84cc0642ec8637c9e07bcf0439422
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 19:56:21 +0000

    glooctl: update 1.8.12 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -17,6 +17,7 @@ class Glooctl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "6cff539c37ebf75a7653796bfab64eefe18aaff537835a7551910d8bbf7744c3"
     sha256 cellar: :any_skip_relocation, catalina:      "581ae7ead5a4529313083c3e628b7a0184545ea9c797f2f6f4bf7a47578282e2"
     sha256 cellar: :any_skip_relocation, mojave:        "0ce167d13103691192d1eb6be43f667190ef57fd54d1af7379b0fe40d14c8a78"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "27da330f0e754ac3e5cacb6433959a191e9b7a1b7be2bde3cdbdf6c0358430b0"
   end
 
   depends_on "go" => :build
