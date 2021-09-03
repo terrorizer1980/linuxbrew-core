68dc1013d7d22d807ffc87c96711c7b04d091b23
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-09-03 06:07:07 +0000

    rosa-cli: update 1.1.2 bottle.

--- Formula/rosa-cli.rb
+++ Formula/rosa-cli.rb
@@ -17,6 +17,7 @@ class RosaCli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "aae37624ab2ac396f1fa448af5d2444a2c42d5fc9accd4cf70088fee949da51a"
     sha256 cellar: :any_skip_relocation, catalina:      "de19e2ac01291f7376a35652b3f0f3a20c449eb5956c414eaca929a81c89206e"
     sha256 cellar: :any_skip_relocation, mojave:        "1168434b6c283d9e8a48ed38c53c325d4d27cae5f73a47df2bb9c604d2a0460a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "100c41e65be701c189cd73b6b589b860c9d24b34e4cff5a6c9561ed416504f4f"
   end
 
   depends_on "go" => :build
